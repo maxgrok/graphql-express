7f0c5e53997b2c6260a2e8186a6b4b2474b6bed0
Max Goodman <max.goodman@flatironschool.com>
2019-02-11 17:29:50 -0500

    adding ) to app.use

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
 const expressGraphQL = require('express-graphql');
 
 const app = express();
 
 app.use('/graphql', expressGraphQL({
 	graphiql: true //only intended for use in dev environment
-});
+}));
 
 app.listen(4000, ()=>{
 	console.log('Listening');
