407d13ee3cd3f0e5f381db7bf52d50bebb94b0ed
Max Goodman <max.goodman@flatironschool.com>
2019-02-11 17:47:06 -0500

    outlining schema

---
+++ schema/schema.js
@@ -0,0 +1,7 @@
+//contains all of the knowledge for telling graphql exacty what the apps data looks like, what properties each object has and how it's related to each other
+
+//user has a firstName, reference to a company and position, and an id
+
+//company has a id, name, description
+
+//position has a id, name and description
