3cb19c09bebbdba84b39e60e8fdedff8e15a9a60
Max Goodman <max.goodman@flatironschool.com>
2019-02-11 17:28:06 -0500

    adding graphiql and using express to do so

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const expressGraphQL = require('express-graphql');
 
 const app = express();
 
+app.use('/graphql', expressGraphQL({
+	graphiql: true
+});
+
 app.listen(4000, ()=>{
 	console.log('Listening');
 })
