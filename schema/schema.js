b3284c4371ec050860a0f1dfb5d297c1fad1ebf1
Max Goodman <max.goodman@flatironschool.com>
2019-02-12 06:16:16 -0500

    edits for the editUser mutation

--- schema/schema.js
+++ schema/schema.js
@@ -113,8 +113,8 @@ const mutation = new GraphQLObjectType({
                 age: {type: GraphQLString},
                 companyId: {type: GraphQLString}
             },
-                resolve(parentValue, {id, firstName, age, companyId}){
-                    return axios.patch(`http://localhost:3000/users/${id}`, {firstName, age, companyId })
+                resolve(parentValue, args){
+                    return axios.patch(`http://localhost:3000/users/${args.id}`, args)
                     .then(res => res.data)
                 }
             }
