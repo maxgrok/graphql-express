609f9024071efcb4c1c88fe6976742751fd446b9
Max Goodman <max.goodman@flatironschool.com>
2019-02-11 19:30:07 -0500

    implementing axios

--- schema/schema.js
+++ schema/schema.js
@@ -7,7 +7,7 @@
 //position has a id, name and description
 
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 
 const {
     GraphQLObjectType, //correct capitalization is important //instructs graphql of the presence of a user (id, firstName)
@@ -16,10 +16,6 @@ const {
     GraphQLSchema //takes in rootquery and returns a schema
 } = graphql;
 
-const users = [//adding hard coded data for resolve
-    { id: '23', firstName: 'Bill', age: 20 },
-    { id: '47', firstName: 'Samantha', age: 21 }
-]
 const UserType = new GraphQLObjectType({ 
         name: 'User', //string describing the type you define. capitalize the name string User
         fields: { //most important property here, tells graphql all the properties that the user has
@@ -43,7 +39,7 @@ const RootQuery = new GraphQLObjectType({ // graphql object type just like user 
 
                 // first argument == never really being used 
                 // second argument args were passed into the original query 
-                return _.find(users, {id: args.id }); // returns a raw javascript object or JSON for resolve functions, walking through all users, find and return the first user that has an id that was given to the resolve function
+                return axios.get(`http://localhost:3000/users/${args.id}`) //axios is a replacement for fetch
                 
             } // give me the id of the user you are looking for then return the user you are looking for. args specified what arguments are required for the root query
         }
